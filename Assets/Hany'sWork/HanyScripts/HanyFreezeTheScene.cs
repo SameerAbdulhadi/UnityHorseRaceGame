4b9a803239e4d7d62ffc7b8b3f3056c9ab8ca278
HanyMajed <hmsweidan@gmail.com>
2023-12-30 01:08:03 +0300

    Volcano map edits
    game start timer
    Spawning power ups in round 2
    colliders fixed
    round 3

---
+++ Assets/Hany'sWork/HanyScripts/HanyFreezeTheScene.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HanyFreezeTheScene : MonoBehaviour
+{
+    public Sprite countImage0;
+    public Sprite countImage1;
+    public Sprite countImage2;
+    public Sprite countImage3;
+    public Image CounterImage; 
+    float duration = 3;
+    float count= 3;
+    AudioSource beepSrc; 
+    public AudioClip beep , siren; 
+    void Start()
+    {
+        beepSrc = GetComponent<AudioSource>();
+        beepSrc.clip = beep;
+        // Call the PauseGame coroutine when the script starts
+        StartCoroutine(FreezeGameForSeconds());
+    }
+
+    IEnumerator FreezeGameForSeconds()
+    {
+        // Pause the game  
+        Time.timeScale = 0f;
+
+        while (count >= 0)
+        {
+
+            Countdown();
+            yield return new WaitForSecondsRealtime(1f);
+
+            if (count < 0)
+            {
+                beepSrc.clip = siren;
+                beepSrc.Play();
+                CounterImage.enabled = false;
+                break;
+            }
+        }
+
+       
+        // Resume the game  
+        Time.timeScale = 1f;
+    }
+
+    private void Countdown()
+    {
+        if (count == 3)
+        {
+            beepSrc.Play();
+            CounterImage.sprite = countImage3;
+        }
+
+        if (count == 2)
+        {
+            beepSrc.Play();
+            CounterImage.sprite = countImage2;
+        }
+
+        if (count == 1)
+        {
+            beepSrc.Play();
+            CounterImage.sprite = countImage1;
+        }
+
+        if (count == 0)
+        {
+            beepSrc.Play();
+            CounterImage.sprite = countImage0;
+        }
+
+        count--;
+    }
+}
--- Assets/Hany'sWork/HanyScripts/HanyPowerUpsTimer.cs
+++ Assets/Hany'sWork/HanyScripts/HanyPowerUpsTimer.cs
@@ -1,25 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HanyPowerUpsTimer : MonoBehaviour
 {
+    int reservedLocation; 
     private float timer = 0; 
     public List<GameObject> positions = new List<GameObject>();
     public GameObject[] GoodPowerUps = new GameObject[2]; 
     public GameObject[] BadPowerUps = new GameObject[2];
+    public float lapsCounter = 0; 
+    private List<GameObject> AllPowerUps = new List<GameObject>();
+    private bool levelTwoWasCalled = false ;
+    private bool levelThreeWasCalled = false;
+    public Canvas canvas;
 
-
-    private void Start()
+    private void Awake()
     {
-        Invoke("LevelTwo", 36);
+        canvas.enabled = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;       
+        timer += Time.deltaTime;
+
+        // invoke level two 
+
+        if (lapsCounter == 2 && levelTwoWasCalled == false )
+        {
+            LevelTwo();
+            levelTwoWasCalled = true;
+        }
+
+        //invoke level three and clear level two 
+
+        if (lapsCounter == 3 && levelThreeWasCalled == false )
+        {
+            ClearLevelTwo();
+            InvokeRepeating( "LevelThree" , 3 ,5);
+            levelThreeWasCalled = true;
+            
+        }
+
+        //the end action 
+
+        if(lapsCounter==4 && levelThreeWasCalled==true)
+        {
+            GameOver();
+        }
     }
 
     private void LevelTwo()
@@ -30,34 +63,97 @@ public class HanyPowerUpsTimer : MonoBehaviour
             //generate 4 random numbers , two of them for the positions childs , and two of them are for choosing the power ups. 
             int goodPositionsRandom = Random.Range(0, 3);
             int goodPowerRandom = Random.Range(0, 2);
-
+             reservedLocation = goodPositionsRandom; 
+            
             // instantiate the good power ups. 
             GameObject instantiated = Instantiate(GoodPowerUps[goodPowerRandom], positions[i].transform.GetChild(goodPositionsRandom).position,
                 positions[i].transform.GetChild(goodPositionsRandom).rotation);
             instantiated.transform.Translate(0,60,0);
-        }
+            AllPowerUps.Add(instantiated);
 
-
-
-        for (int i = 0; i < 6; i++)
-        {
+            // instantiate the bad powers: 
+            int badPositionsRandom;
+            int badPositionsRandom2;
             //generate 4 random numbers , two of them for the positions childs , and two of them are for choosing the power ups. 
-            int badPositionsRandom = Random.Range(0, 3);
+            do
+            {
+                badPositionsRandom = Random.Range(0, 3);
+            } while (badPositionsRandom == reservedLocation);
+
+            do
+            {
+                badPositionsRandom2 = Random.Range(0, 3);
+            } while (badPositionsRandom2 == reservedLocation || badPositionsRandom2 == badPositionsRandom);
+
             int badPowerRandom = Random.Range(0, 2);
+            int badPowerRandom2 = Random.Range(0, 2);
 
             //instantiate the bad power ups. 
-            GameObject instantiated = Instantiate(BadPowerUps[badPowerRandom], positions[i].transform.GetChild(badPositionsRandom).position,
+            GameObject instantiated2 = Instantiate(BadPowerUps[badPowerRandom], positions[i].transform.GetChild(badPositionsRandom).position,
                     positions[i].transform.GetChild(badPositionsRandom).rotation);
-            instantiated.transform.Translate(0, 60, 0);
+            instantiated2.transform.Translate(0, 60, 0);
+            AllPowerUps.Add(instantiated2);
+
+            GameObject instantiated3 = Instantiate(BadPowerUps[badPowerRandom2], positions[i].transform.GetChild(badPositionsRandom2).position,
+                   positions[i].transform.GetChild(badPositionsRandom2).rotation);
+            instantiated3.transform.Translate(0, 60, 0);
+            AllPowerUps.Add(instantiated3);
+
         }
 
     }
 
+    private void ClearLevelTwo ()
+    {
+        // destroy all the power ups in the scene. 
+
+        foreach (GameObject g in AllPowerUps)
+        {
+            Destroy(g);
+        }
+    }
+
     private void LevelThree()
     {
-        if (timer >= 60)
+        StartCoroutine(ImageController());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Horse")
         {
-            print("Level three");
+            if(timer>0 && timer <=10)
+            {
+                lapsCounter = 1;
+            }
+
+            if(timer>=20)
+            {
+                lapsCounter = 2;
+            }
+
+            if (timer>=50)
+            {
+                lapsCounter = 3;
+            }
+
+            if(timer >= 70)
+            {
+                lapsCounter = 4;
+            }
         }
+    }
+
+    private IEnumerator ImageController ()
+    {
+        canvas.enabled = true;
+        yield return new WaitForSeconds(4);
+        canvas.enabled = false;
+    }
+
+    private void GameOver ()
+    {
+        canvas.enabled = false;
+        Time.timeScale = 0;
     }
 }
