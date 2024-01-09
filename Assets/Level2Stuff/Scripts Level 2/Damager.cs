c33f7fd62358c3af2373cf8403bade2afdad084a
hanannj <hanan.pa2002@gmail.com>
2024-01-09 15:44:06 +0300

    life and livesmanager scripts

--- Assets/Level2Stuff/Scripts Level 2/Damager.cs
+++ Assets/Level2Stuff/Scripts Level 2/Damager.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TouchBox : MonoBehaviour
+public class Damager : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
             Life life = other.GetComponent<Life>();
             if (life != null)
-                life.amount -= 5;
+                life.amount -= 1;
     }
     // Start is called before the first frame update
     void Start()
